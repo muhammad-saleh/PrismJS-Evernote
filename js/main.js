09ad2ba04f038216be1313c87d86cb02d553aab5
Muhammad Saleh <mohamed.ahmed-saleh@vodafone.com>
2015-03-13 23:07:51 +0200

    Clicking on the format code button will now select the formatted cde

--- js/main.js
+++ js/main.js
@@ -7,4 +7,18 @@ $(function(){
 		codeOutput.empty().append('<pre class="language-javascript"><code class="language-javascript">'+code+'</code></pre>');
 		Prism.highlightAll();
 	});
+
+	$('.copyCode').on('click',function(e){
+		e.preventDefault();
+        SelectText($('.codeContainer')[0]);
+	});
+
+	function SelectText(element) {
+	    var text = element;
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+	}
 })
